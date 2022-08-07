c16226e412aea72ec757469d75f7dc090cdaad5f
fikma <ade.fm97@gmail.com>
2022-08-07 20:03:38 +0700

    remov inheritedWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:petris/pages/gamePage.dart';
-import 'package:petris/pages/widget/gamePageInheritedWidget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: GamePageInheritedWidget(child: GamePage()),
+      home: GamePage(),
     );
   }
 }
